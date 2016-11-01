f28a3c4c627317d2b16d23a5a00b618327f7c9b9
luisproject <vuongluis@gmail.com>
2016-11-01 21:43:39 +0700

    ok

--- Login01/app/src/main/java/vn/udn/dut/login01/MainActivity.java
+++ Login01/app/src/main/java/vn/udn/dut/login01/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                Intent intent = new Intent(MainActivity.this,SignupActivity.class);
                 startActivity(intent);
             }
         });
